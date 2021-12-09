a24b00f046896a53f172ddd666f9daa3e364da70
Daniil Tikhonov <86834831+DotDaniil@users.noreply.github.com>
2021-12-09 15:27:34 +0300

    Clear-React-Webpack-Project

---
+++ src/Header.jsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+            <h1>This is a configured React webpack project!</h1>
+        </header>
+    );
+}
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const NODE_ENV = process.env.NODE_ENV;
+const IS_DEV = NODE_ENV === 'development';
+
+module.exports = {
+    resolve: {
+        extensions: ['.js', '.jsx', 'js', 'jsx', 'json']
+    },
+    mode: NODE_ENV ? NODE_ENV : 'development',
+    entry: path.resolve(__dirname, 'src/index.jsx'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index.js'
+    },
+    module: {
+        rules: [{
+            test: /\.[tj]sx?$/,
+            use: ['ts-loader']
+        }]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({ template: path.resolve(__dirname, 'index.html') }),
+        // new webpack.DefinePlugin({
+        //     NODE_ENV: JSON.stringify(NODE_ENV)
+        // })
+    ],
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean:true,
+    },
+    devServer: {
+        port: 3000,
+        open: true,
+        hot: IS_DEV
+    }
+};
